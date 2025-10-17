a002bcdb3f0415a31d17f6d0ee5d1f0a1f5fc93c
Andrii Kopylets <andr.kopylets@gmail.com>
2025-10-17 15:37:46 +0300

    #504_edit_relationships_update_migration_and_seeder

--- database/migrations/2025_10_01_151425_create_users_table.php
+++ database/migrations/2025_10_01_151425_create_users_table.php
@@ -1,22 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('party_id')->nullable()->constrained('parties')->onDelete('set null');
             $table->uuid()->nullable()->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable()->default(null);
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->rememberToken();
+
+            $table->text('two_factor_secret')->nullable();
+            $table->text('two_factor_recovery_codes')->nullable();
+            $table->timestamp('two_factor_confirmed_at')->nullable();
+
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->jsonb('settings')->nullable();
@@ -24,14 +30,11 @@ return new class extends Migration
             $table->boolean('is_blocked')->nullable();
             $table->string('block_reason')->nullable();
             $table->foreignId('person_id')->nullable()->constrained('persons')->onDelete('set null');
-            $table->rememberToken();
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
