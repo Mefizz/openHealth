675fe0c7cb7d22788341909c2d7dc6b5832635b9
Andrii Kopylets <andr.kopylets@gmail.com>
2025-10-31 14:13:45 +0200

    i516_update_templates_structure

--- resources/lang/uk/forms.php
+++ resources/lang/uk/forms.php
@@ -206,7 +206,8 @@ return [
 
     // Position & Division
     'position' => 'Посада',
-    'add_position' => 'Додати посаду',
+    'positions' => 'Посади',
+    'add_position' => 'Нова посада',
     'select_position' => 'Обрати посаду',
     'role' => 'Роль',
     'add_role' => 'Додати роль',
