05433e157ff3a81734366a83b7582592f08ba7b9
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2025-11-05 11:02:52 +0200

    #522 feat: add creating page with policy, validation and local saving

---
+++ resources/lang/uk/equipments.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'new' => 'Нове обладнання',
+    'name_medical_product' => 'Назва медичного виробу',
+    'name_type' => 'Тип назви',
+    'type_medical_device' => 'Тип медичного виробу',
+    'serial_number' => 'Серійний №',
+    'recorder' => 'Співробітник, що вносить дані',
+    'additional_data' => 'Додаткові дані',
+    'inventory_number' => 'Інвентарний №',
+    'manufacturer' => 'Виробник',
+    'manufacture_date' => 'Дата виробництва',
+    'expiration_date' => 'Термін придатності',
+    'model_number' => '№ моделі',
+    'lot_number' => '№ закупівлі',
+    'notes_and_comments' => 'Примітки та коментарі',
+
+    'status' => [
+        'active' => 'Активний',
+        'inactive' => 'Неактивний',
+        'entered_in_error' => 'Внесено помилково',
+        'draft' => 'Чернетка'
+    ],
+
+    'availability_status' => [
+        'label' => 'Доступність',
+        'available' => 'Доступний',
+        'damaged' => 'Пошкоджений',
+        'destroyed' => 'Не підлягає відновленню',
+        'lost' => 'Відсутній'
+    ],
+
+    'type' => [
+        'patient_reported' => 'Вказана пацієнтом',
+        'registered' => 'Зареєстрована',
+        'user_friendly' => 'Користувацька',
+    ]
+];
