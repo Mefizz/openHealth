fa417808cfa028be91a51f0a664308848f0d7350
Natalia <nataliakulyk123@gmail.com>
2025-11-06 10:23:39 +0200

    #532 update sidebar

--- resources/views/livewire/components/sidebar.blade.php
+++ resources/views/livewire/components/sidebar.blade.php
@@ -175,6 +175,17 @@
                         </a>
                     </li>
                 @endif
+
+                    @can('viewAny', Equipment::class)
+                        <li>
+                            <a href="{{ route('equipment.index', [legalEntity()]) }}"
+                               class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                            >
+                                @icon('equipment', 'w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white')
+                                <span class="ml-3">{{ __('equipment.equipment') }}</span>
+                            </a>
+                        </li>
+                    @endcan
             @endif
         </ul>
     </div>
