c74e45f39a77bf4c934ad02175c5ef2fef3bce60
Natalia <nataliakulyk123@gmail.com>
2025-11-24 16:05:26 +0200

    #556 update sidebar

--- resources/views/livewire/components/sidebar.blade.php
+++ resources/views/livewire/components/sidebar.blade.php
@@ -147,31 +147,31 @@
                                     <a href="{{ route('employee.index', [legalEntity()]) }}"
                                        class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                                     >
+                                        @icon('positions', 'w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white')
                                         <span class="ml-3">{{ __('forms.positions') }}</span>
                                     </a>
                                 </li>
+
+                                    <li>
+                                        <a href="{{ route('employee-role.index', [legalEntity()]) }}"
+                                           class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                                        >
+                                            @icon('users-roles', 'w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white')
+                                            <span class="ml-3">{{ __('employee-roles.label') }}</span>
+                                        </a>
+                                    </li>
 
                                 <li>
                                     <a href="{{ route('party.verification.index', [legalEntity()]) }}"
                                        class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                                     >
+                                        @icon('verifications', 'w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white')
                                         <span class="ml-3">{{ __('forms.verifications') }}</span>
                                     </a>
                                 </li>
                             </ul>
                         </li>
                     @endif
-
-                @can('viewAny', EmployeeRole::class)
-                    <li>
-                        <a href="{{ route('employee-role.index', [legalEntity()]) }}"
-                           class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                        >
-                            @icon('users-roles', 'w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white')
-                            <span class="ml-3">{{ __('employee-roles.label') }}</span>
-                        </a>
-                    </li>
-                @endif
 
                 @can('viewAny', Contract::class)
                     <li>
