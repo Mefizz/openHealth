586833f3222de24078ea6e3c298927436d497f39
Natalia <nataliakulyk123@gmail.com>
2026-01-08 16:07:39 +0200

    #681 fix problems

--- resources/views/livewire/person/parts/modals/choose-auth-method.blade.php
+++ resources/views/livewire/person/parts/modals/choose-auth-method.blade.php
@@ -18,32 +18,204 @@
             <div x-transition @click="showAuthMethodModal = false" class="modal-wrapper">
                 <div @click.stop
                      x-trap.noscroll.inert="showAuthMethodModal"
-                     class="modal-content w-full max-w-4xl mx-auto bg-white dark:bg-slate-900 p-6 rounded-lg"
+                     class="modal-content w-full max-w-4xl mx-auto"
                 >
-                    <legend class="legend mb-8 text-2xl font-bold">{{ __('patients.changing_SMS_method') }}</legend>
+                    @if($authStep === 0)
+                        <div wire:key="auth-step-0">
+                            <legend class="legend mb-8">{{ __('Методи автентифікації') }}</legend>
 
-                    <div class="bg-gray-100 dark:bg-slate-800 rounded-lg p-4 mb-8 flex items-start">
-                        @icon('alert-circle', 'w-5 h-5 text-gray-700 dark:text-gray-300 mr-3 mt-0.5')
-                        <p class="text-sm text-gray-800 dark:text-gray-200">
-                            {{ __('patients.please_clarify_phone_number') }}
-                            <span class="font-bold">+38095123xxxx</span>
-                        </p>
-                    </div>
+                            <template x-if="!authenticationMethods || authenticationMethods.length === 0">
+                                <div class="bg-red-100 rounded-lg mb-8">
+                                    <div class="p-4">
+                                        <div class="flex items-center gap-2 mb-2">
+                                            @icon('alert-circle', 'w-5 h-5 text-red-700')
+                                            <p class="font-semibold text-red-700">{{ __('forms.patient_has_no_auth_methods') }}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </template>
 
+                            <template x-if="authenticationMethods && authenticationMethods.length > 0">
+                                <div class="space-y-4">
+                                    <template x-for="method in authenticationMethods" :key="method.uuid">
+                                        <div class="fieldset border dark:border-white p-3 rounded space-y-3">
+                                            <div class="flex items-start justify-between">
+                                                <div class="shrink">
+                                                    <template x-if="method.type === 'OFFLINE'">
+                                                        <h3 class="text-gray-900 dark:text-white font-bold">
+                                                            Автентифікація через документи
+                                                        </h3>
+                                                    </template>
 
-                    <div class="mt-8 flex gap-3">
-                        <button type="button" @click="showAuthMethodModal = false" class="button-primary-outline">
-                            {{ __('patients.back_authentication_methods') }}
-                        </button>
+                                                    <template x-if="method.type === 'OTP'">
+                                                        <h3 class="text-gray-900 dark:text-white font-bold">
+                                                            Автентифікація через СМС
+                                                        </h3>
+                                                    </template>
 
-                        <button type="button" @click="showAuthMethodModal = false" class="button-primary-outline-red">
-                            {{ __('patients.no_access') }}
-                        </button>
+                                                    <template x-if="method.type === 'THIRD_PERSON'">
+                                                        <h3 class="text-gray-900 dark:text-white font-bold">
+                                                            Через законного представника
+                                                        </h3>
+                                                    </template>
+                                                </div>
 
-                        <button type="button" @click="showAuthMethodModal = false" class="button-primary">
-                            {{ __('patients.available_access') }}
-                        </button>
-                    </div>
+                                                <div class="shrink-0">
+                                                    <button wire:click="update"
+                                                            class="button-primary whitespace-nowrap"
+                                                            @click="selectedMethod = method.id || method.uuid; showAuthMethodModal = false"
+                                                    >
+                                                        {{ __('forms.select') }}
+                                                    </button>
+                                                </div>
+                                            </div>
+
+                                            <div class="space-y-2">
+                                                <template x-if="method.type === 'OTP'">
+                                                    <div class="space-y-4">
+                                                        <label for="phoneNumber" class="label-modal">
+                                                            {{ __('forms.phone_number') }}
+                                                        </label>
+                                                        <div class="form-row-3">
+                                                            <input type="tel"
+                                                                   class="input-modal"
+                                                                   :value="method.phoneNumber"
+                                                                   id="phoneNumber"
+                                                                   readonly
+                                                            >
+                                                        </div>
+                                                    </div>
+                                                </template>
+
+                                                <template x-if="method.type === 'THIRD_PERSON'">
+                                                    <div class="space-y-4">
+                                                        <div class="form-row-2">
+                                                            <div class="form-group"
+                                                                 x-data="{ endedAt: method.ehealthEndedAt || method.endedAt }"
+                                                            >
+                                                                @icon('calendar-month', 'w-5 h-5 svg-input absolute left-1 !top-2/3 transform -translate-y-1/2 pointer-events-none')
+
+                                                                <label for="endedAt" class="label-modal">
+                                                                    {{ __('patients.ended_at') }}
+                                                                    <span class="text-red-600"></span>
+                                                                </label>
+                                                                <input x-model="endedAt"
+                                                                       x-init="$nextTick(() => { if (endedAt) $el.value = endedAt })"
+                                                                       datepicker-max-date="{{ now()->format('d.m.Y') }}"
+                                                                       datepicker-format="dd.mm.yyyy"
+                                                                       type="text"
+                                                                       name="endedAt"
+                                                                       id="endedAt"
+                                                                       class="input-modal datepicker-input"
+                                                                       autocomplete="off"
+                                                                       readonly
+                                                                >
+                                                            </div>
+
+                                                            <div class="form-group">
+                                                                <label for="confidantPersonFullName" class="label-modal">
+                                                                    {{ __('patients.confidant_full_name') }}
+                                                                </label>
+                                                                <input type="text"
+                                                                       :value="method.confidantPerson.name"
+                                                                       class="input-modal"
+                                                                       name="confidantPersonFullName"
+                                                                       readonly
+                                                                >
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="form-row-2">
+                                                            <div class="form-group">
+                                                                <label for="taxId" class="label-modal">
+                                                                    {{ __('forms.rnokpp') }}
+                                                                    <span class="text-red-600"></span>
+                                                                </label>
+                                                                <input :value="method.confidantPerson.taxId"
+                                                                       type="text"
+                                                                       name="taxId"
+                                                                       id="taxId"
+                                                                       class="input-modal"
+                                                                       autocomplete="off"
+                                                                       readonly
+                                                                >
+                                                            </div>
+
+                                                            <div class="form-group">
+                                                                <label for="unzr" class="label-modal">
+                                                                    {{ __('patients.unzr') }}
+                                                                </label>
+                                                                <input type="text"
+                                                                       :value="method.confidantPerson.unzr"
+                                                                       class="input-modal"
+                                                                       name="unzr"
+                                                                       readonly
+                                                                >
+                                                            </div>
+                                                        </div>
+
+                                                        <template x-for="document in method.confidantPerson.documentsPerson"
+                                                                  :key="method.uuid"
+                                                        >
+                                                            <div class="form-row-2">
+                                                                <div class="form-group"
+                                                                     x-data="{
+                                                                     documentLabels: @js(__('patients.documents')),
+                                                                     getDocumentLabel(type) {
+                                                                         return this.documentLabels[type?.toLowerCase()] ?? type
+                                                                     }
+                                                                 }"
+                                                                >
+                                                                    <label for="taxId" class="label-modal">
+                                                                        {{ __('forms.document_type') }}
+                                                                        <span class="text-red-600"></span>
+                                                                    </label>
+                                                                    <input :value="getDocumentLabel(document.type)"
+                                                                           type="text"
+                                                                           name="taxId"
+                                                                           id="taxId"
+                                                                           class="input-modal"
+                                                                           autocomplete="off"
+                                                                           readonly
+                                                                    >
+                                                                </div>
+
+                                                                <div class="form-group">
+                                                                    <label for="documentNumber" class="label-modal">
+                                                                        {{ __('forms.document_number') }}
+                                                                    </label>
+                                                                    <input type="text"
+                                                                           :value="document.number"
+                                                                           class="input-modal"
+                                                                           name="documentNumber"
+                                                                           readonly
+                                                                    >
+                                                                </div>
+                                                            </div>
+                                                        </template>
+                                                    </div>
+                                                </template>
+                                            </div>
+                                        </div>
+                                    </template>
+                                </div>
+                            </template>
+
+                            <div class="flex justify-between items-center mt-8">
+                                <button type="button" @click="showAuthMethodModal = false" class="button-minor">
+                                    {{ __('forms.cancel') }}
+                                </button>
+
+                                <button type="button" wire:click.prevent="setStep(1)" class="button-primary">
+                                    {{ __('Змінити метод автентифікації') }}
+                                </button>
+                            </div>
+                        </div>
+                    @else
+                        <div wire:key="auth-step-{{ $authStep }}">
+                            @include('livewire.person.parts.modals.choose-auth-method-' . $authStep)
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
